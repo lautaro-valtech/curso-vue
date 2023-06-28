703a3c542dccb41c437a428786522da02565a9b4
Lautaro <lauticor@ubuntu-pc.fibertel.com.ar>
2023-06-28 07:32:26 -0300

    end section 4

---
+++ 03-fundamentos/src/components/Counter.vue
@@ -0,0 +1,68 @@
+<template>
+  <h2>{{ customTitle }}</h2>
+  <p>
+    {{ counter }}
+    <sup>2</sup>
+    = {{ squareCounter }}
+  </p>
+  <div>
+    <button @click="increment">+1</button>
+    <button @click="decrement">-1</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String,
+    start: {
+      type: Number,
+      default: 100,
+      required: true,
+      validator(value) {
+        return value >= 100;
+      },
+    },
+  },
+  name: 'Counter',
+  components: {},
+  data() {
+    return {
+      counter: this.start,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter--;
+    },
+  },
+  computed: {
+    squareCounter() {
+      return this.counter * this.counter;
+    },
+    customTitle() {
+      return this.title || 'Counter';
+    },
+  },
+};
+</script>
+
+<style>
+button {
+  background-color: #64b687;
+  border-radius: 5px;
+  border: 1px solid white;
+  color: white;
+  cursor: pointer;
+  margin: 0px 5px;
+  padding: 5px 15px;
+  transition: 0.3s ease-in-out;
+}
+button:hover {
+  background-color: #5aa67b;
+  transition: 0.3s ease-in-out;
+}
+</style>
