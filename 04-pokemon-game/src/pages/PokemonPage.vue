e3e3fb3550e4a7fd92689338b5d2a5d4f8fad8c8
lautaro-valtech <lautaro.coria@valtech.com>
2023-06-28 08:55:04 -0300

    start section 7

---
+++ 04-pokemon-game/src/pages/PokemonPage.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1>¿Quién es este Pokémon?</h1>
+  <PokemonPicture />
+  <PokemonOptions />
+</template>
+
+<script>
+import PokemonOptions from '@/components/PokemonOptions';
+import PokemonPicture from '@/components/PokemonPicture';
+
+export default {
+  components: {
+    PokemonOptions,
+    PokemonPicture,
+  },
+};
+</script>
+
+<style></style>
